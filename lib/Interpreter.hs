b75be36e584ea02cb4de855256c54ee726a49540
John Cinnamond <john@cinnamond.me.uk>
2023-04-06 21:58:13 +0100

    Add TimeValue type

--- lib/Interpreter.hs
+++ lib/Interpreter.hs
@@ -1,21 +1,35 @@
-module Interpreter (eval, run) where
+module Interpreter (eval, run, Value (..)) where
 
 import Data.Text as T
+import Data.Time (LocalTime, defaultTimeLocale)
+import Data.Time.Format (parseTimeM)
 import Parser (Expr (..), parse)
 
-run :: Text -> Text
+data Value
+    = TextValue Text
+    | TimeValue LocalTime
+    deriving stock (Show, Eq)
+
+instance IsString Value where
+    fromString = TextValue . T.pack
+
+run :: Text -> Value
 run inp = case parse inp of
-    Left err -> "error: " <> show err
+    Left err -> TextValue $ "error: " <> show err
     Right e -> eval e
 
-eval :: Expr -> Text
-eval e = go e ""
+eval :: Expr -> Value
+eval e = go e Nothing
   where
-    go :: Expr -> Text -> Text
+    go :: Expr -> Maybe Value -> Value
     go (Call fn args) x = callBuiltin fn args x
-    go (Pipe c exprs) x = go exprs $ go c x
+    go (Pipe c exprs) x = go exprs $ Just (go c x)
 
-callBuiltin :: Text -> [Text] -> Text -> Text
-callBuiltin "uuid" [] = const "123-456"
-callBuiltin "strip" [x] = T.replace x ""
-callBuiltin _ _ = undefined
+callBuiltin :: Text -> [Text] -> Maybe Value -> Value
+callBuiltin "uuid" [] Nothing = TextValue "123-456"
+callBuiltin "strip" [x] (Just (TextValue v)) = TextValue $ T.replace x "" v
+callBuiltin "parseTime" [x] Nothing =
+    case parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%s%Ez" (T.unpack x) of
+        Nothing -> error "bad time format"
+        Just v -> TimeValue v
+callBuiltin _ _ _ = undefined
--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -1,8 +1,9 @@
 module InterpreterSpec (spec) where
 
-import Interpreter (eval)
+import Data.Time (LocalTime, defaultTimeLocale, parseTimeM)
+import Interpreter (Value (..), eval)
 import Parser (Expr (..))
-import Test.Hspec (Spec, it, shouldBe)
+import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
@@ -11,3 +12,8 @@ spec = do
 
     it "interprets a pipe" $
         eval (Pipe (Call "uuid" []) (Call "strip" ["-"])) `shouldBe` "123456"
+
+    describe "commands" $ do
+        it "handles parseTime" $ do
+            let Just expectedTime = parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%s%Ez" "2023-04-06T21:14:00+01:00" :: Maybe LocalTime
+            eval (Call "parseTime" ["2023-04-06T21:14:00+01:00"]) `shouldBe` TimeValue expectedTime
