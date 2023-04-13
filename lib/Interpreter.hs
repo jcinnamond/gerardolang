03732eede11a0bf1e345c8d4123816189bd7ed25
John Cinnamond <john@cinnamond.me.uk>
2023-04-13 21:07:56 +0100

    Add void type, remove unnecessary Maybe

--- lib/Interpreter.hs
+++ lib/Interpreter.hs
@@ -8,6 +8,7 @@ import Parser (Expr (..), parse)
 data Value
     = TextValue Text
     | TimeValue LocalTime
+    | Void
     deriving stock (Show, Eq)
 
 instance IsString Value where
@@ -19,16 +20,16 @@ run inp = case parse inp of
     Right e -> eval e
 
 eval :: Expr -> Value
-eval e = go e Nothing
+eval e = go e Void
   where
-    go :: Expr -> Maybe Value -> Value
+    go :: Expr -> Value -> Value
     go (Call fn args) x = callBuiltin fn args x
-    go (Pipe c exprs) x = go exprs $ Just (go c x)
+    go (Pipe c exprs) x = go exprs $ go c x
 
-callBuiltin :: Text -> [Text] -> Maybe Value -> Value
-callBuiltin "uuid" [] Nothing = TextValue "123-456"
-callBuiltin "strip" [x] (Just (TextValue v)) = TextValue $ T.replace x "" v
-callBuiltin "parseTime" [x] Nothing =
+callBuiltin :: Text -> [Text] -> Value -> Value
+callBuiltin "uuid" [] Void = TextValue "123-456"
+callBuiltin "strip" [x] (TextValue v) = TextValue $ T.replace x "" v
+callBuiltin "parseTime" [x] Void =
     case parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%s%Ez" (T.unpack x) of
         Nothing -> error "bad time format"
         Just v -> TimeValue v
